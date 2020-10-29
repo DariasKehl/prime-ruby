16693114acc2c455fb76e86916c4ee5b64ee7256
DariasKehl <neocount@gmail.com>
2020-10-29 12:59:54 -0400

    commit tree 598e3d38e9a980f275fa68ac42adccb9142b6b82

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(num)
+  sieve = (0..num).to_a
+  ## holy fuck this makes a lot of output puts sieve
+  sieve[0]=nil
+  sieve[1]=nil
+  max_num = num
+
+  (num/2+1).times do |n|
+    if(sieve[n]!=nil) then
+        cnt=2*n
+        while cnt <= max_num do
+            sieve[cnt]=nil
+            cnt+=n
+        end
+    end
+  end
+  sieve.compact!
+  return sieve.include?(num)
+end
